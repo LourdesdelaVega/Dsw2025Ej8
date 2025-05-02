5cf8ae7dbdcf1144e7d64cf7a4faaba7c4655a7f
Rocio de la Vega <delavegalourdes61@gmail.com>
2025-05-02 20:51:38 -0300

    Trabajo Finalizado

---
+++ Domain/CajaDeAhorro.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dsw2025Ej8.Ex;
+
+namespace Dsw2025Ej8.Domain
+{
+    public class CajaDeAhorro : CuentaBancaria
+    {
+        public CajaDeAhorro(string numero, decimal saldo, string[] titulares)
+            : base(numero, saldo, TipoCuenta.CajaDeAhorro, titulares)
+        {
+
+        }
+
+        public override void Depositar(decimal monto)
+        {
+            ValidarMonto(monto);
+            ValidarEstado(EstadoBancario);
+            Saldo += monto;
+            AplicarInteres();
+
+        }
+        public override void Retirar(decimal monto)
+        {
+            ValidarMonto(monto);
+            ValidarEstado(EstadoBancario);
+
+            Saldo -= monto;
+
+        }
+
+
+        public void AplicarInteres()
+        {
+
+            Saldo += Saldo * TasaDeInteres;
+        }
+
+    }
+
+}
